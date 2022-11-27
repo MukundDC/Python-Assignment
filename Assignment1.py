089847afcb3acfd0787c022c759618f59997afa3
MukundDC <119129249+MukundDC@users.noreply.github.com>
2022-11-27 21:57:02 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,32 @@
+# * is a mathematical operator
+#"hello" is a string 
+# -87.8 is a float
+# - (minus)is a mathematical opertator
+# / (divide) is a mathematical opertator
+#   + (plus) is a mathematical opertator
+# 6 it is a integer
+
+#Q2 - Variable is a letter or word which contains some value assigned to it, where as string is just a word or number which dont have any value.
+#
+#Q3- Three Data types - Integer, Float, String, Complex Number
+
+#Q4- Expression is used to get answer by comaparing or addiing or subtracting etc. 
+#    Expression gives the required condition(True,Greater Than) or operation(Multiply, Minus) we want.
+
+#Q5- In assignment statement (spam=10) we have the answer or no calculation is there, expression is when we calculate or use any mathematical operator in statement(spam = 5*5)
+
+#Q6
+bacon = 22
+bacon + 1 
+print(bacon)  # answer is 22, even though we add bacon will take the same value if in second statement it was bacon=bacon + 1 then bacon will be 23
+
+#Q7
+print('spam'+'spamspam')  #printed spamspamspam
+print('spam'*3)     #printed spamspamspam
+
+#Q8-  egg is valid and 100 is not because variable starts with alphabet
+
+#Q9- For Integer-int(), Str- str(), float- float()
+
+#Q10- Because if consist of string & interger, to fix it we have to type as
+print('i have eaten'+' 99 '+'burritos')
